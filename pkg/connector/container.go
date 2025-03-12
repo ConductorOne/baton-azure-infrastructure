53c5e2101a24f239ca1302964b810b3b07659e17
Marcus Goldschmidt <marcus.golds@hotmail.com>
2025-03-12 15:57:24 -0400

    fix: container list

--- pkg/connector/container.go
+++ pkg/connector/container.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storage/armstorage"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/authorization/armauthorization"
-	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
-	azService "github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/service"
 	"github.com/conductorone/baton-azure-infrastructure/pkg/connector/client"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
@@ -15,8 +15,6 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/types/entitlement"
 	rs "github.com/conductorone/baton-sdk/pkg/types/resource"
 )
-
-var serviceUrlTemplate = "https://%s.blob.core.windows.net/"
 
 // containerBuilder syncs Container given an StorageAccount.
 type containerBuilder struct {
@@ -37,55 +35,68 @@ func (usr *containerBuilder) List(ctx context.Context, parentResourceID *v2.Reso
 		return nil, "", nil, fmt.Errorf("invalid resource type: %s", parentResourceID.ResourceType)
 	}
 
-	storageAccountIdSplit := strings.Split(parentResourceID.Resource, ":")
-
-	if len(storageAccountIdSplit) != 5 {
-		return nil, "", nil, fmt.Errorf("invalid resource id: %s", parentResourceID.Resource)
-	}
-
-	storageAccountName := storageAccountIdSplit[len(storageAccountIdSplit)-1]
-
-	serviceUrl := fmt.Sprintf(serviceUrlTemplate, storageAccountName)
-
-	azBlobClient, err := usr.client.AzBlobClient(serviceUrl)
+	storageId, err := newStorageResourceSplitIdDataFromConnectorId(parentResourceID.Resource)
 	if err != nil {
 		return nil, "", nil, err
 	}
+
+	factory, err := armstorage.NewClientFactory(
+		storageId.subscriptionID,
+		usr.conn.token,
+		usr.conn.client.ArmOptions(),
+	)
+
+	if err != nil {
+		return nil, "", nil, err
+	}
+
+	pager := factory.NewBlobContainersClient().
+		NewListPager(
+			storageId.resourceGroupName,
+			storageId.resourceName,
+			nil,
+		)
 
 	resources := make([]*v2.Resource, 0)
 
-	var options *azblob.ListContainersOptions
-	if pToken.Token != "" {
-		options = &azblob.ListContainersOptions{
-			Marker: &pToken.Token,
+	for pager.More() {
+		result, err := pager.NextPage(ctx)
+		if err != nil {
+			return nil, "", nil, err
 		}
+
+		temp, err := ConvertErr(result.Value, func(container *armstorage.ListContainerItem) (*v2.Resource, error) {
+			profile := map[string]interface{}{
+				"type":                    StringValue(container.Type),
+				"has_immutability_policy": BoolValue(container.Properties.HasImmutabilityPolicy),
+				"has_legal_hold":          BoolValue(container.Properties.HasLegalHold),
+			}
+
+			if container.Properties.PublicAccess != nil {
+				profile["properties_public_access"] = string(*container.Properties.PublicAccess)
+			}
+
+			appTraits := []rs.AppTraitOption{
+				rs.WithAppProfile(profile),
+			}
+
+			return rs.NewResource(
+				StringValue(container.Name),
+				containerResourceType,
+				fmt.Sprintf("%s:%s", storageId.resourceName, StringValue(container.Name)),
+				rs.WithAppTrait(appTraits...),
+				rs.WithParentResourceID(parentResourceID),
+			)
+		})
+
+		if err != nil {
+			return nil, "", nil, err
+		}
+
+		resources = append(resources, temp...)
 	}
 
-	pager := azBlobClient.NewListContainersPager(options)
-
-	result, err := pager.NextPage(ctx)
-	if err != nil {
-		return nil, "", nil, err
-	}
-
-	convertErr, err := ConvertErr(result.ContainerItems, func(in *azService.ContainerItem) (*v2.Resource, error) {
-		return containerResource(storageAccountName, in, parentResourceID)
-	})
-
-	if err != nil {
-		return nil, "", nil, err
-	}
-
-	resources = append(resources, convertErr...)
-
-	var nextToken string
-	if result.NextMarker != nil && len(*result.NextMarker) > 0 {
-		nextToken = *result.NextMarker
-	} else {
-		nextToken = ""
-	}
-
-	return resources, nextToken, nil, nil
+	return resources, "", nil, nil
 }
 
 // Entitlements always returns an empty slice for users.
@@ -141,30 +152,6 @@ func (usr *containerBuilder) Grants(ctx context.Context, resource *v2.Resource, 
 	}
 
 	return grants, "", nil, nil
-}
-
-func containerResource(storageAccountName string, container *azService.ContainerItem, parentResourceID *v2.ResourceId) (*v2.Resource, error) {
-	profile := map[string]interface{}{
-		"version":                 StringValue(container.Version),
-		"has_immutability_policy": BoolValue(container.Properties.HasImmutabilityPolicy),
-		"has_legal_hold":          BoolValue(container.Properties.HasLegalHold),
-	}
-
-	if container.Properties.PublicAccess != nil {
-		profile["properties_public_access"] = string(*container.Properties.PublicAccess)
-	}
-
-	appTraits := []rs.AppTraitOption{
-		rs.WithAppProfile(profile),
-	}
-
-	return rs.NewResource(
-		StringValue(container.Name),
-		containerResourceType,
-		fmt.Sprintf("%s:%s", storageAccountName, StringValue(container.Name)),
-		rs.WithAppTrait(appTraits...),
-		rs.WithParentResourceID(parentResourceID),
-	)
 }
 
 func newContainerBuilder(conn *Connector) *containerBuilder {
