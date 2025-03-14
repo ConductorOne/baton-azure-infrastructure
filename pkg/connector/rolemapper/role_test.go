dd4279a3eaa35242647e1939c901bf43d7c7319e
Marcus Goldschmidt <marcus.golds@hotmail.com>
2025-03-14 12:04:11 -0400

    Fix containr permissions

--- pkg/connector/rolemapper/role_test.go
+++ pkg/connector/rolemapper/role_test.go
@@ -3,6 +3,8 @@ package rolemapper
 import (
 	"testing"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/authorization/armauthorization"
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -38,4 +40,64 @@ func TestRoleMapper(t *testing.T) {
 
 	action = mapper.GetRoleAction("*")
 	require.Len(t, action, 3)
+}
+
+func TestMapRoleToAzureRoleAction(t *testing.T) {
+	toPointerString := func(s string) *string {
+		return &s
+	}
+
+	cases := []struct {
+		name        string
+		mapper      *AzureRoleActionMapper
+		permissions []*armauthorization.Permission
+		expected    []string
+	}{
+		{
+			name:   "empty permissions",
+			mapper: ContainerPermissions,
+			permissions: []*armauthorization.Permission{
+				{
+					Actions:    nil,
+					NotActions: nil,
+				},
+			},
+			expected: nil,
+		},
+		{
+			name:   "all scope",
+			mapper: ContainerPermissions,
+			permissions: []*armauthorization.Permission{
+				{
+					Actions: []*string{
+						toPointerString("Microsoft.Storage/storageAccounts/blobServices/containers/*"),
+					},
+					NotActions: nil,
+				},
+			},
+			expected: []string{"read", "write", "delete"},
+		},
+		{
+			name:   "all scope 2",
+			mapper: ContainerPermissions,
+			permissions: []*armauthorization.Permission{
+				{
+					Actions: []*string{
+						toPointerString("*"),
+					},
+					NotActions: nil,
+				},
+			},
+			expected: []string{"read", "write", "delete"},
+		},
+	}
+
+	for _, s := range cases {
+		t.Run(s.name, func(t *testing.T) {
+			action, err := s.mapper.MapRoleToAzureRoleAction(s.permissions)
+			require.NoError(t, err)
+
+			require.Len(t, action, len(s.expected))
+		})
+	}
 }
